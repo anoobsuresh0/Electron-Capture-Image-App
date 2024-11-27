5210e3b5c284ad6284a49747c14d309161b86c6d
Anoob Suresh <anoob.suresh@perleybrook.com>
2024-11-27 22:57:00 +0530

    feat: Create clickable menu

--- main.js
+++ main.js
@@ -1,27 +1,77 @@
-// https://www.youtube.com/watch?v=kBtYon8KmdU&list=PLe30vg_FG4OSKH_8zpLlnf4WpNlzL526E&index=3
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Menu, shell } = require("electron");
+const path = require("path");
 
-const createWindow = ()=>{
-    const win = new BrowserWindow({
-        height:600,
-        width:800,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-          }
-    })
-    win.loadFile('index.html');
-}
+const menuItems = [
+  {
+    label: app.name,
+    submenu: [
+      {
+        label: "About",
+        click: () => {
+          console.log("About clicked");
+        },
+      },
+      { type: "separator" },
+      {
+        label: "Quit",
+        accelerator: "CmdOrCtrl+Q",
+        click: () => {
+          app.quit();
+        },
+      },
+    ],
+  },
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Exit",
+        click: () => app.quit(),
+      },
+      { type: "separator" },
+      {
+        label: "Learn More",
+        click: async () => {
+          await shell.openExternal("https://www.electronjs.org/");
+        },
+      },
+    ],
+  },
+  {
+    label: "Window",
+    submenu: [
+      { role: "minimize" },
+      {
+        role: "close",
+      },
+    ],
+  },
+];
 
-app.whenReady().then(()=>{
-    createWindow();
-    app.on('activate',()=>{
-        if(BrowserWindow.getAllWindows().length === 0){
-            createWindow();
-        }
-    })
+const menu = Menu.buildFromTemplate(menuItems);
+Menu.setApplicationMenu(menu);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
